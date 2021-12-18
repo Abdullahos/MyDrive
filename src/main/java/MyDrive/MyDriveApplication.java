486dbe45f11b1764981e67b3d6c9efbe251fd628
Abdullah Osama <abdullahelagami55@gmail.com>
2021-12-18 13:07:02 +0200

    initial commit

---
+++ src/main/java/MyDrive/MyDriveApplication.java
@@ -0,0 +1,13 @@
+package MyDrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDriveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MyDrive/MyDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package MyDrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
