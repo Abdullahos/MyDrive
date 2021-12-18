534f911324b5dec1202dd7c88a92ed4b134d5e5b
Abdullah Osama <abdullahelagami55@gmail.com>
2021-12-18 13:35:29 +0200

    handles 404 not found

--- src/main/java/MyDrive/controller/ExceptionHandling/PageNotFoundController.java
+++ src/main/java/MyDrive/controller/ExceptionHandling/PageNotFoundController.java
@@ -1,4 +1,4 @@
-package com.Mydrive.MyDrive.ExceptionHandling;
+package MyDrive.controller.ExceptionHandling;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
