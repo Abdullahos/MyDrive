77a6468e4edad871dace24d99ce6bcade6ce394d
Abdullah Osama <abdullahelagami55@gmail.com>
2021-12-18 13:18:54 +0200

    create user repo

--- src/main/java/MyDrive/Repository/UserRepository.java
+++ src/main/java/MyDrive/Repository/UserRepository.java
@@ -1,7 +1,6 @@
-package com.Mydrive.MyDrive.Repository;
+package MyDrive.Repository;
 
-import com.Mydrive.MyDrive.models.Note;
-import com.Mydrive.MyDrive.models.User;
+import MyDrive.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
